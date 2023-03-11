37bc696632d04c0020914865d6914f049d6051b1
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-03-11 16:54:06 -0300

    adicionando a pagina de filmes

--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -2,7 +2,19 @@ interface IVideoProps extends React.VideoHTMLAttributes<HTMLVideoElement> {}
 import "./video.style.css";
 
 const Video = (props: IVideoProps) => {
-	return <video controls className="container-video" {...props}></video>;
+	return (
+		<video
+			preload="auto"
+			x-webkit-airplay="allow"
+			webkit-playsinline="true"
+			id="vjs_video_2548_html5_api"
+			tabIndex={-1}
+			autoPlay={true}
+			playsInline
+			className="container-video"
+			{...props}
+		></video>
+	);
 };
 
 export { Video };
