d96e7b879df18930a8f09789fb7398729944a26a
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-03-11 19:12:56 -0300

    update

--- src/components/video/video.tsx
+++ src/components/video/video.tsx
@@ -1,33 +1,21 @@
-import { useRef } from "react";
-import Plyr, { APITypes } from "plyr-react";
-import "./video.style.css";
-import "plyr-react/plyr.css";
-
 interface IVideoProps extends React.VideoHTMLAttributes<HTMLVideoElement> {}
-const videoId = "yWtFb9LJs3o";
-const provider = "html5";
-const videoOptions = undefined;
+import "./video.style.css";
 
 const Video = (props: IVideoProps) => {
-	const ref = useRef<APITypes>(null);
-
-	const plyrVideo =
-		videoId && provider ? (
-			<Plyr
-				ref={ref}
-				source={{
-					type: "video",
-					sources: [
-						{
-							src: props.src as string,
-							provider: provider,
-						},
-					],
-				}}
-				options={videoOptions}
-			/>
-		) : null;
-	return <div className="container-video">{plyrVideo}</div>;
+	return (
+		<video
+			preload="auto"
+			x-webkit-airplay="allow"
+			webkit-playsinline="true"
+			id="vjs_video_2548_html5_api"
+			tabIndex={-1}
+			autoPlay={true}
+			playsInline
+			controls
+			className="container-video"
+			{...props}
+		></video>
+	);
 };
 
 export { Video };
