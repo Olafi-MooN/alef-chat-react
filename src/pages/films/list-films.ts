046d7a63434c9a4aeef1d7f1eb49b785543ba3b1
Alef Santos Soares <54686408+Olafi-MooN@users.noreply.github.com>
2023-03-12 22:28:42 -0300

    update

--- src/pages/films/list-films.ts
+++ src/pages/films/list-films.ts
@@ -6,7 +6,7 @@ const listOfFilms = () => {
 			genre: "Comedia",
 			title: "Como Se Fosse a Primeira Vez",
 			imdbRating: "6.8",
-			link: "https://2475154694.tapecontent.net/radosgw/yx6ga3bMdrs1MvB/ffyfznqC74SkIyJ5IuDDZmRxNMN2DJoDOhyaJ9x9mGF6CAr56UVixjX3gaZNLpqd58frTZBV9g8mAVDTcTVSO29vxQ062lBTOlLgT7mTavTmOY9jQWe7jq60HHVvzBjdLRzNrM4cuenTEXHr6Xm8HJ2syVVkuddKBErTpG2f8PmVXTvUcCp9fZKVG19RWJBw9FAsUXhrEJPi3q9SfqO1PT2MWUxub5s5fwdpfXu3snUmmFx-AmeLyDG44dcrdLGZpJ1czumMYY4gcxtG8B6XdmLwnL15LsPz-7IQT57Fx41OpgZ3uAUHTdHxZic/SLUKeNmI5w3sNEvi.mp4?stream=1",
+			link: "//streamtape.com/get_video?id=yx6ga3bMdrs1MvB&expires=1678740672&ip=FRuOExyOR0gN&token=29Lp2ZfjUg4q&stream=1",
 			thumb: "https://vizer.in/content/movies/posterPt/342/20817.webp",
 			year: "2004",
 		},
